eba5b2f6af5686643a11ff76a65780bf6ee67dd2
housiadas <housiadas@gmail.com>
2017-01-10 14:08:43 +0200

    part 2c

---
+++ part2C.m
@@ -0,0 +1,26 @@
+% part 2C
+
+clear;
+clc;
+GroupData;
+Patterns = GroupPatterns';
+
+global IW distances;
+
+minimum = min(Patterns);
+minimum = minimum';
+maximum = max(Patterns);
+maximum = maximum';
+minMax = [minimum maximum];
+gridSize = [8 8];
+somCreate(minMax, gridSize);
+
+setOrderLR = 0.9;
+setOrderSteps = 500;
+setTuneLR = 0.1;
+somTrainParameters(setOrderLR, setOrderSteps, setTuneLR);
+
+somTrain(Patterns);
+
+figure; plot2DSomData(IW, distances, Patterns);
+figure;somShow(IW,gridSize);
