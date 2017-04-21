63d7e8d25c0d66e87d8d4e6fca76625ba7b1720b
Dimitris Housiadas <housiadas@gmail.com>
2017-04-21 16:53:39 +0300

    some deletions

--- alex/somOutput.m
+++
@@ -1,9 +0,0 @@
-function  [output]  = somOutput( pattern )
-    global IW ;
-    
-    % IW => NxD
-    % pattern => Dx1
-
-  output=compet(negdist(IW,pattern'));
-
-end
--- marga/somUpdate.m
+++
@@ -1,7 +0,0 @@
-function somUpdate( patterns, learningRate, neighborDist )
-    global IW ;
-    a = somActivation( patterns, neighborDist ); %a=1x100
-    for i=1:size(IW,1)
-        IW(i,:) = IW(i,:) + learningRate*a(i)*(patterns - IW(i,:));
-    end 
-end
